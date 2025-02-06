c877bacd5e516e13ab7cc2375ba9b61be27e80b4
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2025-02-07 00:46:13 +0500

    html revision

---
+++ Simple-Web-Page/app.js
@@ -0,0 +1,9 @@
+// console.log("Nothing special!")
+
+const btn = document.querySelector("button");
+
+let formSubmit = btn.addEventListener("click", (evt) => {
+    // console.log(evt);
+    console.log("Button was clicked!")
+
+})
