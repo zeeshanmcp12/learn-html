8517b0e189d925289d3d68ea3eaf23330045dc42
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2025-02-07 23:03:26 +0500

    simple form handling

--- Simple-Web-Page/app.js
+++ Simple-Web-Page/app.js
@@ -2,8 +2,21 @@
 
 const btn = document.querySelector("button");
 
-let formSubmit = btn.addEventListener("click", (evt) => {
-    // console.log(evt);
-    console.log("Button was clicked!")
+const inputVal = document.getElementById("name");
 
-})
+// let formSubmit = btn.addEventListener("click", (evt) => {
+//     // console.log(evt);
+//     console.log("Button was clicked!")
+
+// })
+
+function formSubmit(){
+    btn.addEventListener("click", () => {
+        console.log("Button was clicked!")
+        console.log(`Hi ${inputVal.value}!`);
+
+    })
+}
+
+// console.log(typeof formSubmit);
+// console.log(inputVal)
